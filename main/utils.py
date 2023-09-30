8e370987375383ee48e199cfebad9cab8b75aad4
Niccolò Perego <niccolo.perego@mail.polimi.it>
2023-09-30 23:16:34 +0200

    changes for py3.7

--- main/utils.py
+++ main/utils.py
@@ -4,22 +4,22 @@ import sys
 from typing import NoReturn, Tuple
 
 
-def printer(__msg: str, /) -> None:
+def printer(__msg: str) -> None:
     print(f'[{datetime.datetime.now().strftime("%H:%M:%S")}] {__msg}')
 
-def error(__msg: str, /) -> NoReturn:
+def error(__msg: str) -> NoReturn:
     printer(f'Error: {__msg}')
     sys.exit(1)
 
 def convert(hex):
     return hex.replace('#', '0x')
 
-def hex_to_rgb(hex: str, /) -> Tuple[int, int, int]:
+def hex_to_rgb(hex: str) -> Tuple[int, int, int]:
     return tuple(
         int(hex[i:i + 2], 16)
         for i in (1, 3, 5)
     )
 
-def validate_hex(hex: str, /):
+def validate_hex(hex: str):
     if not re.match(r'^#[a-fA-F0-9]{6}$', hex):
         error(f'Invalid hexcode: {hex}')
